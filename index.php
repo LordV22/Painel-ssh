8643e971d1adff03850c578822c9bd39ff85ae4a
LordV22 <lordv22@users.noreply.github.com>
2026-07-11 03:03:00 +0000

    Fix: serve from root directory, redirect to /gestorssh/

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-header("Location: /gestorssh/");
-exit;
+// Verifica se está na raiz e redireciona para o gestorssh
+if (strpos($_SERVER['REQUEST_URI'], '/gestorssh/') === false) {
+    header("Location: /gestorssh/");
+    exit;
+}
